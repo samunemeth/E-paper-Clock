88ace1e4dff30a6288bef928052343e7e523d567
Samu Németh <nemeth.samu.0202@gmail.com>
2025-06-05 10:48:45 +0200

    Updated gitignore

--- reporting-server/index.js
+++ reporting-server/index.js
@@ -1,30 +1,45 @@
-const express = require('express');
-var serveIndex = require('serve-index');
-const fs = require('fs/promises');
+import express from "express";
+import serveIndex from "serve-index";
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from 'url';
+
 const app = express();
 const port = 12891;
 
-const path = require('path');
+// Get absolute paths.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Use json formatting to get reported data.
 app.use(express.json());
 
 // Serve the static main page.
-app.use('/', express.static(path.join(__dirname, 'public')));
+const publicPath = path.join(__dirname, "./public");
+app.use("/", express.static(publicPath));
 
 // Server the collected data.
-app.use('/data', express.static(path.join(__dirname, 'data')));
-app.use('/data', serveIndex(path.join(__dirname, 'data')));
+const dataPath = path.join(__dirname, "./data");
+app.use("/data", express.static(dataPath));
+app.use("/data", serveIndex(dataPath));
+app.use("/data", (req, res, next) => {
+    res.set("Content-Disposition", "inline");
+    res.set("Content-Type", "text/plain");
+    next();
+});
 
 
 // Get the data from the reports.
-app.post('/report', async (req, res) => {
+app.post("/report", async (req, res) => {
     const data = req.body;
     console.log(`Received the following data: ${JSON.stringify(data)}`);
     const timestamp = (new Date()).toISOString();
-    try { await fs.mkdir('./data'); } catch (error) { };
-    await fs.appendFile(`./data/${data.uuid}.csv`, `${timestamp}; ${data.batteryLevel}\n`, 'utf8');
+    await fs.appendFile(`./data/${data.uuid}.log`, `${timestamp}; ${data.batteryLevel}\n`, "utf8");
     res.sendStatus(200);
 });
+
+// Create the date folder if needed.
+try { await fs.mkdir("./data"); } catch (error) { };
 
 // Start the server.
 app.listen(port, () => {
